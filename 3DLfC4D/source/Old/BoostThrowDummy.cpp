0712d7280a30b6c7a1cefb8a1115dc8b5454dbba
FilipMalmberg <filip.malmberg@it.uu.se>
2018-11-14 09:08:02 +0100

    Remove legacy code

--- 3DLfC4D/source/Old/BoostThrowDummy.cpp
+++
@@ -1,8 +0,0 @@
-//#pragma warning(push)
-#pragma warning(disable : 4265)
-#include <boost/throw_exception.hpp>
-//#pragma warning(pop)
-
-
-void boost::throw_exception(std::exception const& e){
-}
--- 3DLfC4D/source/Old/BoostThrowImpl.cpp
+++
@@ -1,7 +0,0 @@
-
-#pragma warning(disable : 4265)
-#include <boost/throw_exception.hpp>
-
-
-void boost::throw_exception(std::exception const& e){
-}
