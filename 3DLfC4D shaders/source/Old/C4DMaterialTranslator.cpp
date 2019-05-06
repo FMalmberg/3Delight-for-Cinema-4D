9cb832424ecb87aed14a704b9fae891504486dbe
Filip Malmberg <pfmalmberg@gmail.com>
2019-05-06 21:46:52 +0200

    Removing old code

--- 3DLfC4D shaders/source/Old/C4DMaterialTranslator.cpp
+++
@@ -1,5 +0,0 @@
-#include "C4DMaterialTranslator.h"
-
-void C4DMaterialTranslator::CreateNSINodes(const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser){
-	GePrint("C4d material translator");
-}
