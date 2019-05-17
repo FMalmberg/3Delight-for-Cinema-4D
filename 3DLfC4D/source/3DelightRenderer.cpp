FMalmberg/3Delight-for-Cinema-4D

commit 7eea8d8407c46689cf181bdc12c0896bfed264a6

-Refactoring Scene parser in preparation for live rendering -Introduce "DL_SampleInfo" class to hold information about motion samples. This causes some changes to the functin signatures for translators and hooks. -Fixed functionality of the "motion blur" checkbox. -Fixed asynchronous rendering - rendering no longer freezes cinema 4d GUI.

3DLfC4D/source/3DelightRenderer.cpp
@@ -218,6 +218,7 @@ Bool Register3DelightPlugin(void)
 	RegisterIcon(DL_JPG_ON, GeGetPluginResourcePath() + "jpg-on.png");
 	RegisterIcon(DL_JPG_OFF, GeGetPluginResourcePath() + "jpg.png");
 
-	return RegisterVideoPostPlugin(ID_RENDERSETTINGS, "3Delight Renderer"_s, PLUGINFLAG_VIDEOPOST_ISRENDERER, RenderSettings::Alloc, "myres"_s, 0, 0);
+	return RegisterVideoPostPlugin(ID_RENDERSETTINGS, "3Delight"_s, 0, RenderSettings::Alloc, "myres"_s, 0, 0); 
+	//PLUGINFLAG_VIDEOPOST_ISRENDERER
 }
 

3DLfC4D/source/SceneParser.cpp
@@ -69,6 +69,8 @@ void RenderProgress( float i_progress, void *data ){
 
 
 
+
+
 vector<float> GetSamples(double open, double close, int samples){
 	vector<float> values(samples);
 	if(samples==1){
@@ -87,19 +89,25 @@ SceneParser::~SceneParser(){}
 SceneParser::SceneParser(){
 	context_handle=NSI_BAD_CONTEXT;
 	
-	render_doc = NULL;
+	doc = NULL;
 
-	settings=NULL;
-	stage=NONE;
+	//settings=NULL;
+	//stage=NONE;
 
 	rendermode = PREVIEW_RENDER;
 	nMotionSamples=1;
 	shutterOpen=0;
 	shutterClose=0;
 	shutterTime = 0;
-	ExportAOVs=false;
+	//ExportAOVs=false;
 
 	UpdateMotionSampleTimes();
+}
+
+SceneParser::SceneParser(BaseDocument* document, NSIContext_t context) {
+	context_handle = context;
+	doc = document;
+	rendermode = PREVIEW_RENDER;
 }
 
 void SceneParser::RequestMinimumShutterTime(float t){
@@ -116,15 +124,156 @@ NSIContext_t SceneParser::GetContext(){
 	return context_handle;
 }
 
-void SceneParser::SetAOVExport(bool export_aovs){
+/*void SceneParser::SetAOVExport(bool export_aovs){
 	ExportAOVs=export_aovs;
+}*/
+
+void SceneParser::FillRenderSettings() {
+	if (!doc) { return; }
+
+	RenderData* rd = doc->GetActiveRenderData();
+	BaseContainer* render_data = rd->GetDataInstance();
+	BaseVideoPost* vp = rd->GetFirstVideoPost();
+
+	bool has_vp = false;
+	while (vp != NULL && !has_vp) {
+		has_vp = (vp->GetType() == ID_RENDERSETTINGS); //Look for rendersettings
+		if (!has_vp) {
+			vp = vp->GetNext();
+		}
+	}
+
+	if (!has_vp) { //If there are not rendersettings in the document, create them
+		BaseVideoPost* pvp = BaseVideoPost::Alloc(ID_RENDERSETTINGS);
+		rd->InsertVideoPostLast(pvp);
+
+		vp = rd->GetFirstVideoPost();
+		while (vp != NULL && !has_vp) {
+			has_vp = (vp->GetType() == ID_RENDERSETTINGS); //Look for rendersettings
+			if (!has_vp) {
+				vp = vp->GetNext();
+			}
+		}
+
+	}
+
+	if (has_vp) {
+		settings = vp->GetData();
+	}
+	
 }
 
+bool SceneParser::InitScene(bool animate, long frame) {
+	if (!doc) { return false; }
 
-bool SceneParser::Parse(BaseDocument* doc, long frame){
+	FillRenderSettings();
+
+	//Motion sampling settings
+	bool useMotionBlur = settings.GetBool(DL_MOTION_BLUR);
+	long motionSamples = 2; // settings.GetInt32(DL_MOTION_SAMPLES, 2);
+	if (!useMotionBlur) { motionSamples = 1; }
+
+	float ShutterAngle = settings.GetFloat(DL_SHUTTER_ANGLE,0.5);
+	long fps = doc->GetFps();
+	float shutterOpen_t = float(frame) / float(fps);
+	float shutterClose_t = float(frame + ShutterAngle) / float(fps);
+
+	SetMotionSamples(motionSamples);
+	SetShutter(shutterOpen_t, shutterClose_t);
+
+	if (animate) {
+		AnimateDoc(doc, shutterOpen);
+	}
+
+	//Clear previous scene info
+	transforms.clear();
+	nodes.clear();
+	hooks.clear();
+
+	//Create nodes and transforms
+
+	//Traverse objects
+	TraverseScene(doc->GetFirstObject(), doc, ".root");
+
+	//Traverse materials 
+	BaseMaterial* mat = doc->GetFirstMaterial();
+	while (mat) {
+		//Create translator for material
+		Node n(mat);
+		DL_Translator* translator = n.GetTranslator();
+		if (translator) {
+			nodes.push_back(n);
+			translator->CreateNSINodes("", mat, doc, this);
+		}
+
+		//Traverse sub-shaders
+		BaseShader* shader = mat->GetFirstShader();
+		TraverseShaders(shader, doc);
+
+		mat = mat->GetNext();
+	}
+
+	//Create hooks
+	hooks = PM.GetHooks();
+	for (long i = 0; i<hooks.size(); i++) {
+		hooks[i]->CreateNSINodes(doc, this);
+	}
+
+	//Make connections
+	for (long i = 0; i<nodes.size(); i++) {
+		DL_Translator* translator = nodes[i].GetTranslator();
+		if (translator) {
+			translator->ConnectNSINodes(nodes[i].GetC4DNode(doc), doc, this);
+		}
+	}
+
+	for (long i = 0; i<hooks.size(); i++) {
+		hooks[i]->ConnectNSINodes(doc, this);
+	}
+
+	return true;
+
+}
+
+void SceneParser::SampleFrameMotion() {
+	DL_SampleInfo info;
+	info.samples_max = nMotionSamples;
+	info.shutter_open_time = shutterOpen;
+	info.shutter_close_time = shutterClose;
+
+	for (long s = 0; s<nMotionSamples; s++) {
+
+		info.sample = s;
+		info.sample_time = motionSampleTimes[s];
+
+		AnimateDoc(doc, motionSampleTimes[s]);
+		for (long i = 0; i<hooks.size(); i++) {
+			hooks[i]->SampleMotion(&info, doc, this);
+		}
+
+		for (long i = 0; i<transforms.size(); i++) {
+			transforms[i].SampleMotion(&info, doc, this);
+		}
+
+		DL_Translator* translator;
+		for (long i = 0; i<nodes.size(); i++) {
+			translator = nodes[i].GetTranslator();
+			BaseList2D* c4d_node = nodes[i].GetC4DNode(doc);
+			if (translator && c4d_node) {
+				translator->SampleMotion(&info, nodes[i].GetC4DNode(doc), doc, this);
+			}
+		}
+
+		//SampleMotion(i, doc);
+	}
+
+
+}
+
+bool SceneParser::Parse(BaseDocument* document, long frame){
 	if (!doc){ return false; }
 
-	render_doc = doc;
+	doc = doc;
 
 	RenderData* rd = doc->GetActiveRenderData();
 	BaseContainer* render_data = rd->GetDataInstance();
@@ -141,9 +290,9 @@ bool SceneParser::Parse(BaseDocument* doc, long frame){
 	
 
 
-	settings = NULL;
-	if (has_vp){ settings = vp->GetDataInstance(); }
-	if (settings == NULL){ return false; }
+	//settings = NULL;
+	//if (has_vp){ settings = vp->GetDataInstance(); }
+	//if (settings == NULL){ return false; }
 
 
 
@@ -157,11 +306,11 @@ bool SceneParser::Parse(BaseDocument* doc, long frame){
 	param[0].type = NSITypePointer; param[0].count = 1;
 	param[0].flags = 0; param[0].arraylength = 0;
 	
-	if(settings->GetString(DL_ISCLICKED) == "Render")
+	if(settings.GetString(DL_ISCLICKED) == "Render")
 	context.Begin(NSI::PointerArg("errorhandler", NSIErrorHandlerC4D));
 
-	else if (settings->GetString(DL_ISCLICKED) == "Export") {
-		String flnm = settings->GetFilename(DL_FOLDER_OUTPUT).GetString();
+	else if (settings.GetString(DL_ISCLICKED) == "Export") {
+		String flnm = settings.GetFilename(DL_FOLDER_OUTPUT).GetString();
 		std::string exported = flnm.GetCStringCopy();
 		context.Begin(NSI::StringArg("streamfilename", exported.c_str()));
 	}
@@ -169,11 +318,11 @@ bool SceneParser::Parse(BaseDocument* doc, long frame){
 	context_handle = context.Handle();
 
 	//Motion sampling settings
-	bool useMotionBlur=settings->GetBool(DL_USE_MOTION_BLUR);
-	long motionSamples=settings->GetInt32(DL_MOTION_SAMPLES,2);
+	bool useMotionBlur=settings.GetBool(DL_USE_MOTION_BLUR);
+	long motionSamples=settings.GetInt32(DL_MOTION_SAMPLES,2);
 	if(!useMotionBlur){ motionSamples=1; }
 
-	float ShutterAngle=settings->GetFloat(DL_SHUTTER_ANGLE);
+	float ShutterAngle=settings.GetFloat(DL_SHUTTER_ANGLE);
 	long fps=doc->GetFps();
 	float shutterOpen_t=float(frame)/float(fps);
 	float shutterClose_t=float(frame+ShutterAngle)/float(fps);
@@ -184,7 +333,7 @@ bool SceneParser::Parse(BaseDocument* doc, long frame){
 	AnimateDoc(doc,shutterOpen);
 
 
-	stage=BUILD_SCENE_TREE;
+	//stage=BUILD_SCENE_TREE;
 	
 	//Create nodes and transforms
 	transforms.clear();
@@ -251,7 +400,7 @@ bool SceneParser::Parse(BaseDocument* doc, long frame){
 		));
 
 	context_handle=NSI_BAD_CONTEXT;
-	stage=NONE;
+	//stage=NONE;
 	//return (progress>99.9); 
 	return (renderstatus == NSIRenderCompleted);
 }
@@ -291,7 +440,7 @@ int SceneParser::GetMotionSamples(){
 }
 
 BaseContainer* SceneParser::GetSettings(){
-	return settings;
+	return &settings;
 }
 
 void SceneParser::UpdateMotionSampleTimes(){
@@ -358,11 +507,11 @@ const char* SceneParser::GetAssociatedHandle(BaseList2D* node){
 void SceneParser::SampleMotion(long s, BaseDocument* doc){
 	
 	for(long i=0; i<hooks.size(); i++){
-		hooks[i]->SampleMotion(motionSampleTimes[s], s,doc,this);
+		//hooks[i]->SampleMotion(motionSampleTimes[s], s,doc,this);
 	}
 	
 	for(long i=0; i<transforms.size(); i++){
-		transforms[i].SampleMotion(motionSampleTimes[s],s,doc,this);
+		//transforms[i].SampleMotion(motionSampleTimes[s],s,doc,this);
 	}
 
 	DL_Translator* translator;
@@ -370,7 +519,7 @@ void SceneParser::SampleMotion(long s, BaseDocument* doc){
 		translator=nodes[i].GetTranslator();
 		BaseList2D* c4d_node = nodes[i].GetC4DNode(doc);
 		if(translator && c4d_node){ 
-			translator->SampleMotion(motionSampleTimes[s], s,nodes[i].GetC4DNode(doc), doc,this);
+			//translator->SampleMotion(motionSampleTimes[s], s,nodes[i].GetC4DNode(doc), doc,this);
 		}
 	}
 }

3DLfC4D geometry/source/BaseObjectTranslator.h
@@ -9,7 +9,7 @@
 class BaseObjectTranslator : public DL_TranslatorPlugin {
 public:
 	virtual void CreateNSINodes(const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
-	virtual void SampleMotion(double t, long i, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
+	virtual void SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
 	PolygonObject* ParseObjectToPolygon(BaseObject* obj);
 };
 #endif // !BASEOBJECTTRANSLATOR_H_

3DLfC4D geometry/source/PolygonObjectTranslator.cpp
@@ -258,7 +258,7 @@ void PolygonObjectTranslator::CreateNSINodes(const char* ParentTransformHandle, 
 
 }
 
-void PolygonObjectTranslator::SampleMotion(double t, long i, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser){
+void PolygonObjectTranslator::SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser){
 	if (skip){ return; }
 
 	NSI::Context ctx(parser->GetContext());
@@ -321,7 +321,7 @@ void PolygonObjectTranslator::SampleMotion(double t, long i, GeListNode* C4DNode
 	arg_P.SetCount(pointcount);
 	arg_P.SetValuePointer((void*)&P[0]);
 
-	ctx.SetAttributeAtTime(handle, t, (
+	ctx.SetAttributeAtTime(handle, info->sample_time, (
 		arg_P
 		));
 
@@ -352,7 +352,7 @@ void PolygonObjectTranslator::SampleMotion(double t, long i, GeListNode* C4DNode
 		arg_N.SetFlags(NSIParamPerVertex);
 
 
-		ctx.SetAttributeAtTime(handle, t, (
+		ctx.SetAttributeAtTime(handle, info->sample_time, (
 			arg_N
 			));
 	}

3DLfC4D geometry/source/PolygonObjectTranslator.h
@@ -12,7 +12,7 @@ class PolygonObjectTranslator : public DL_TranslatorPlugin{
 
 public:
 	virtual void CreateNSINodes(const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
-	virtual void SampleMotion(double t, long i, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser);
+	virtual void SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser);
 
 private:
 	bool skip;

3DLfC4D lights/source/EnvironmentLightTranslator.cpp
@@ -79,7 +79,7 @@ void EnvironmentLightTranslator::CreateNSINodes(const char* ParentTransformHandl
 
 }
 
-void EnvironmentLightTranslator::SampleMotion(double t, long i, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser){
+void EnvironmentLightTranslator::SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser){
 	
 	NSI::Context ctx(parser->GetContext());
 

3DLfC4D lights/source/EnvironmentLightTranslator.h
@@ -8,7 +8,7 @@
 class EnvironmentLightTranslator : public DL_TranslatorPlugin{
 public:
 	virtual void CreateNSINodes(const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
-	virtual void SampleMotion(double t, long i, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser);
+	virtual void SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser);
 
 	//virtual void CacheStaticData(BaseObject* obj, BaseDocument* doc, DL_SceneParser* parser);
 	//virtual void Emit(BaseObject* obj, BaseDocument* doc, DL_SceneParser* parser);

3DLfC4D lights/source/LightCardTranslator.cpp
@@ -83,7 +83,7 @@ void LightCardTranslator::CreateNSINodes(const char* ParentTransformHandle, GeLi
 
 }
 
-void LightCardTranslator::SampleMotion(double t, long i, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser) {
+void LightCardTranslator::SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser) {
 
 	NSI::Context ctx(parser->GetContext());
 
@@ -120,7 +120,7 @@ void LightCardTranslator::SampleMotion(double t, long i, GeListNode* C4DNode, Ba
 	arg_P.SetValuePointer((void*)&P[0]);
 	//arg_P.SetFlags(NSIParamIndirect);
 
-	ctx.SetAttributeAtTime(handle, t, (
+	ctx.SetAttributeAtTime(handle, info->sample_time, (
 		arg_P
 		));
 
@@ -137,7 +137,7 @@ void LightCardTranslator::SampleMotion(double t, long i, GeListNode* C4DNode, Ba
 	col[1] = color.y;
 	col[2] = color.z;
 
-	ctx.SetAttributeAtTime(shader_handle, t, (
+	ctx.SetAttributeAtTime(shader_handle, info->sample_time, (
 		NSI::ColorArg("tint", &col[0]),
 		NSI::FloatArg("intensity", intensity)
 		));

commit ae7bfbc1bdfda544031021802f0608282a3f60dc

Fixed Render Settings 'Render' and 'Export to NSI' functionalities
This was broken by commit 7eea8d8407c46689cf181bdc12c0896bfed264a6

3DLfC4D/source/3DelightRenderer.cpp
@@ -9,6 +9,89 @@
 #include "IDs.h"
 #include <assert.h> 
 #include <algorithm>
+#include "SceneParser.h"
+
+void NSIErrorHandlerC4D(void *userdata, int level, int code, const char *message)
+{
+	const char *pre = (const char *)userdata;
+	std::string buffer("3Delight");
+
+	if (pre)
+	{
+		buffer += " (" + std::string(pre) + "): ";
+	}
+	else
+		buffer += ": ";
+
+	buffer += message;
+
+	switch (level)
+	{
+	case NSIErrMessage:
+		ApplicationOutput(buffer.c_str());
+		break;
+	case NSIErrInfo:
+		ApplicationOutput(buffer.c_str());
+		break;
+	case NSIErrWarning:
+		ApplicationOutput(buffer.c_str());
+		break;
+	default:
+	case NSIErrError:
+		ApplicationOutput(buffer.c_str());
+		break;
+	}
+}
+
+
+bool Render(BaseDocument* doc, long frame, RENDER_MODE mode, bool progressive, BaseContainer* settings) {
+	BaseDocument* renderdoc = (BaseDocument*)doc->GetClone(COPYFLAGS::DOCUMENT, nullptr);
+
+	NSIContext_t context_handle = NSIBegin(0, 0);
+	String action = settings->GetString(DL_ISCLICKED);
+	if (action == "Render")
+	{
+		NSIErrorHandler_t eh = NSIErrorHandlerC4D;
+		NSIParam_t streamParam;
+		streamParam.name = "errorhandler";
+		streamParam.data = &eh;
+		streamParam.type = NSITypePointer;
+		context_handle = NSIBegin(1, &streamParam);
+	}
+	else if (action == "Export")
+	{
+		String file = settings->GetFilename(DL_FOLDER_OUTPUT).GetString();
+		std::string exported = file.GetCStringCopy();
+		NSIParam_t streamParam;
+		const char *output = exported.c_str();
+		streamParam.name = "streamfilename";
+		streamParam.data = &output;
+		streamParam.type = NSITypeString;
+
+		context_handle = NSIBegin(1, &streamParam);
+	}
+
+	NSI::Context context(context_handle);
+
+	SceneParser sp(renderdoc, context_handle);
+
+	sp.SetRenderMode(mode);
+
+	//Render scene
+	bool RenderOK = sp.InitScene(true, frame);
+	sp.SampleFrameMotion();
+
+	BaseDocument::Free(renderdoc);
+
+	context.SetAttribute(NSI_SCENE_GLOBAL, (
+		NSI::IntegerArg("renderatlowpriority", 1)
+		));
+
+	context.RenderControl((
+		NSI::StringArg("action", "start")
+		));
+	return RenderOK;
+}
 
 /**
 	Function to hide a specific element from the RenderSettings GUI
@@ -124,11 +207,12 @@ Bool RenderSettings::Message(GeListNode* i_node, Int32 i_type, void* i_data)
 		{
 			DescriptionCommand* dc = (DescriptionCommand*)i_data;
 			BaseContainer* dldata = ((BaseVideoPost*)i_node)->GetDataInstance();
+			BaseDocument* doc = GetActiveDocument();
+			BaseTime t = doc->GetTime();
+			long frame = t.GetFrame(doc->GetFps());
 			if (dc->_descId[0].id == DL_CREATE_RENDER_SETTINGS) //If render button is clicked
 			{
 				dldata->SetString(DL_ISCLICKED, "Render"_s);
-				//Executes this command which will run the plugin with the ID_RENDERFRAME ID.
-				CallCommand(ID_RENDERFRAME);
 			}
 			//If Export to NSI File button is clicked
 			if (dc->_descId[0].id == DL_EXPORT_RENDER_SETTINGS) 
@@ -143,8 +227,8 @@ Bool RenderSettings::Message(GeListNode* i_node, Int32 i_type, void* i_data)
 					folder.FileSelect(FILESELECTTYPE::ANYTHING, FILESELECT::SAVE, "Select Folder"_s);
 					dldata->SetFilename(DL_FOLDER_OUTPUT, folder);
 				}
-				CallCommand(ID_RENDERFRAME);
 			}
+			Render(doc, frame, PREVIEW_RENDER, true,dldata);
 			break;
 		}
 	}
@@ -218,7 +302,7 @@ Bool Register3DelightPlugin(void)
 	RegisterIcon(DL_JPG_ON, GeGetPluginResourcePath() + "jpg-on.png");
 	RegisterIcon(DL_JPG_OFF, GeGetPluginResourcePath() + "jpg.png");
 
-	return RegisterVideoPostPlugin(ID_RENDERSETTINGS, "3Delight"_s, 0, RenderSettings::Alloc, "myres"_s, 0, 0); 
+	return RegisterVideoPostPlugin(ID_RENDERSETTINGS, "3Delight"_s, PLUGINFLAG_VIDEOPOST_ISRENDERER, RenderSettings::Alloc, "myres"_s, 0, 0); 
 	//PLUGINFLAG_VIDEOPOST_ISRENDERER
 }
 

3DLfC4D/source/SceneParser.cpp
@@ -16,38 +16,6 @@
 // std::stringstream, std::stringbuf
 using namespace std;
 
-void NSIErrorHandlerC4D(void *userdata, int level, int code, const char *message)
-{
-	const char *pre = (const char *)userdata;
-	std::string buffer("3Delight");
-
-	if (pre)
-	{
-		buffer += " (" + std::string(pre) + "): ";
-	}
-	else
-		buffer += ": ";
-
-	buffer += message;
-
-	switch (level)
-	{
-	case NSIErrMessage:
-		ApplicationOutput(buffer.c_str());
-		break;
-	case NSIErrInfo:
-		ApplicationOutput(buffer.c_str());
-		break;
-	case NSIErrWarning:
-		ApplicationOutput(buffer.c_str());
-		break;
-	default:
-	case NSIErrError:
-		ApplicationOutput(buffer.c_str());
-		break;
-	}
-}
-
 
 void StoppedCallback(
 	void* stoppedcallbackdata,
@@ -334,18 +302,8 @@ bool SceneParser::Parse(BaseDocument* document, long frame){
 
 	NSI::Context context;
 
-	NSIErrorHandler_t eh = NSIErrorHandlerC4D;
 
-	NSIParam_t param[1];
-	param[0].name = "errorhandler";
-	param[0].data = &eh;
-	param[0].type = NSITypePointer; param[0].count = 1;
-	param[0].flags = 0; param[0].arraylength = 0;
-	
-	if(settings.GetString(DL_ISCLICKED) == "Render")
-	context.Begin(NSI::PointerArg("errorhandler", NSIErrorHandlerC4D));
-
-	else if (settings.GetString(DL_ISCLICKED) == "Export") {
+	if (settings.GetString(DL_ISCLICKED) == "Export") {
 		String flnm = settings.GetFilename(DL_FOLDER_OUTPUT).GetString();
 		std::string exported = flnm.GetCStringCopy();
 		context.Begin(NSI::StringArg("streamfilename", exported.c_str()));