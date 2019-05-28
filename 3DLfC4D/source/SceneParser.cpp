FMalmberg/3Delight-for-Cinema-4D

commit d783c921ae229c123f8f5cd1a276cd4f60b3801d

API changes, in preparation for new NSI handle naming routines.

3DLfC4D/source/SceneParser.cpp
@@ -171,7 +171,7 @@ bool SceneParser::InitScene(bool animate, long frame) {
 		DL_Translator* translator = n.GetTranslator();
 		if (translator) {
 			nodes.push_back(n);
-			translator->CreateNSINodes("", mat, doc, this);
+			translator->CreateNSINodes("","", mat, doc, this);
 		}
 
 		//Traverse sub-shaders
@@ -191,7 +191,7 @@ bool SceneParser::InitScene(bool animate, long frame) {
 	for (long i = 0; i<nodes.size(); i++) {
 		DL_Translator* translator = nodes[i].GetTranslator();
 		if (translator) {
-			translator->ConnectNSINodes(nodes[i].GetC4DNode(doc), doc, this);
+			translator->ConnectNSINodes("",nodes[i].GetC4DNode(doc), doc, this);
 		}
 	}
 
@@ -228,7 +228,7 @@ void SceneParser::SampleFrameMotion() {
 			translator = nodes[i].GetTranslator();
 			BaseList2D* c4d_node = nodes[i].GetC4DNode(doc);
 			if (translator && c4d_node) {
-				translator->SampleMotion(&info, nodes[i].GetC4DNode(doc), doc, this);
+				translator->SampleMotion(&info, "",nodes[i].GetC4DNode(doc), doc, this);
 			}
 		}
 
@@ -265,7 +265,7 @@ void SceneParser::InteractiveUpdate() {
 			nodes[i].dirty_checksum = new_dirtyval;
 			translator = nodes[i].GetTranslator();
 			if (translator && c4d_node) {
-				translator->SampleMotion(&info, nodes[i].GetC4DNode(doc), doc, this);
+				translator->SampleMotion(&info, "", nodes[i].GetC4DNode(doc), doc, this);
 			}
 		}
 	}
@@ -341,7 +341,7 @@ bool SceneParser::Parse(BaseDocument* document, long frame){
 		DL_Translator* translator = n.GetTranslator();
 		if (translator){
 			nodes.push_back(n);
-			translator->CreateNSINodes("", mat, doc, this);
+			translator->CreateNSINodes("","", mat, doc, this);
 		}
 
 		//Traverse sub-shaders
@@ -361,7 +361,7 @@ bool SceneParser::Parse(BaseDocument* document, long frame){
 	for(long i=0; i<nodes.size(); i++){
 		DL_Translator* translator=nodes[i].GetTranslator();
 		if(translator){
-			translator->ConnectNSINodes(nodes[i].GetC4DNode(doc), doc, this);		
+			translator->ConnectNSINodes("",nodes[i].GetC4DNode(doc), doc, this);		
 		}
 	}
 
@@ -457,6 +457,16 @@ vector<float> SceneParser::GetSampleTimes(int nsamples){
 	return times;
 }
 
+const char* SceneParser::GetHandleName(BaseList2D* node) {
+	Int memsize = 0;
+	const Char* mem;
+	node->FindUniqueID(MAXON_CREATOR_ID, mem, memsize);
+	String ID_STR;
+	ID_STR.SetCString(mem, memsize);
+	handle = StringToStdString(ID_STR);
+	return handle.c_str();
+}
+
 std::string SceneParser::GetIDString(BaseList2D* node){
 	Int memsize=0;
 	const Char* mem;
@@ -531,7 +541,7 @@ void SceneParser::TraverseShaders(BaseShader* shader, BaseDocument* doc){
 	DL_Translator* translator = n.GetTranslator();
 	if (translator){
 		nodes.push_back(n);
-		translator->CreateNSINodes("", shader, doc, this);
+		translator->CreateNSINodes("", "",shader, doc, this);
 	}
 
 	TraverseShaders(shader->GetDown(),doc);
@@ -560,7 +570,7 @@ void SceneParser::TraverseScene(BaseObject* obj, BaseDocument* doc,std::string p
 		DL_Translator* translator=n.GetTranslator();
 		if(translator){
 			nodes.push_back(n);
-			translator->CreateNSINodes(transform_handle.c_str(), obj, doc, this);
+			translator->CreateNSINodes("",transform_handle.c_str(), obj, doc, this);
 		}
 	}
 
@@ -571,7 +581,7 @@ void SceneParser::TraverseScene(BaseObject* obj, BaseDocument* doc,std::string p
 		DL_Translator* tagtranslator=tagnode.GetTranslator();
 		if(tagtranslator){
 			nodes.push_back(tagnode);
-			tagtranslator->CreateNSINodes(transform_handle.c_str(), tag, doc, this);
+			tagtranslator->CreateNSINodes("", transform_handle.c_str(), tag, doc, this);
 		}
 		tag=tag->GetNext();
 	}

3DLfC4D geometry/source/BaseObjectTranslator.h
@@ -8,8 +8,8 @@
 #include <map>
 class BaseObjectTranslator : public DL_TranslatorPlugin {
 public:
-	virtual void CreateNSINodes(const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
-	virtual void SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
+	virtual void CreateNSINodes(const char* Handle, const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
+	virtual void SampleMotion(DL_SampleInfo* info, const char* Handle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
 	PolygonObject* ParseObjectToPolygon(BaseObject* obj);
 };
 #endif // !BASEOBJECTTRANSLATOR_H_

3DLfC4D geometry/source/PolygonObjectTranslator.cpp
@@ -64,7 +64,7 @@ PolygonObject* GetMeshFromNode(GeListNode* C4DNode){
 	return mesh;
 }
 
-void PolygonObjectTranslator::CreateNSINodes(const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser){
+void PolygonObjectTranslator::CreateNSINodes(const char* Handle, const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser){
 	NSI::Context ctx(parser->GetContext());
 
 	skip = false;
@@ -258,7 +258,7 @@ void PolygonObjectTranslator::CreateNSINodes(const char* ParentTransformHandle, 
 
 }
 
-void PolygonObjectTranslator::SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser){
+void PolygonObjectTranslator::SampleMotion(DL_SampleInfo* info, const char* Handle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser){
 	if (skip){ return; }
 
 	NSI::Context ctx(parser->GetContext());

3DLfC4D geometry/source/PolygonObjectTranslator.h
@@ -11,8 +11,8 @@
 class PolygonObjectTranslator : public DL_TranslatorPlugin{
 
 public:
-	virtual void CreateNSINodes(const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
-	virtual void SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser);
+	virtual void CreateNSINodes(const char* Handle, const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
+	virtual void SampleMotion(DL_SampleInfo* info, const char* Handle, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser);
 
 private:
 	bool skip;

3DLfC4D lights/source/EnvironmentLightTranslator.cpp
@@ -7,7 +7,7 @@
 #include "nsi.hpp"
 //#include "Util.h"
 
-void EnvironmentLightTranslator::CreateNSINodes(const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser){
+void EnvironmentLightTranslator::CreateNSINodes(const char* Handle, const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser){
 	NSI::Context ctx(parser->GetContext());
 
 	//Create a mesh and connect it to the parent transform
@@ -79,7 +79,7 @@ void EnvironmentLightTranslator::CreateNSINodes(const char* ParentTransformHandl
 
 }
 
-void EnvironmentLightTranslator::SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser){
+void EnvironmentLightTranslator::SampleMotion(DL_SampleInfo* info, const char* Handle, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser){
 	
 	NSI::Context ctx(parser->GetContext());
 

3DLfC4D lights/source/EnvironmentLightTranslator.h
@@ -7,8 +7,8 @@
 
 class EnvironmentLightTranslator : public DL_TranslatorPlugin{
 public:
-	virtual void CreateNSINodes(const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
-	virtual void SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser);
+	virtual void CreateNSINodes(const char* Handle, const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
+	virtual void SampleMotion(DL_SampleInfo* info, const char* Handle, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser);
 
 	//virtual void CacheStaticData(BaseObject* obj, BaseDocument* doc, DL_SceneParser* parser);
 	//virtual void Emit(BaseObject* obj, BaseDocument* doc, DL_SceneParser* parser);

3DLfC4D lights/source/LightCardTranslator.cpp
@@ -11,7 +11,7 @@
 //#include "Util.h"
 
 
-void LightCardTranslator::CreateNSINodes(const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser) {
+void LightCardTranslator::CreateNSINodes(const char* Handle, const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser) {
 	NSI::Context ctx(parser->GetContext());
 
 	//Create a mesh and connect it to the parent transform
@@ -83,7 +83,7 @@ void LightCardTranslator::CreateNSINodes(const char* ParentTransformHandle, GeLi
 
 }
 
-void LightCardTranslator::SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser) {
+void LightCardTranslator::SampleMotion(DL_SampleInfo* info, const char* Handle,  GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser) {
 
 	NSI::Context ctx(parser->GetContext());
 

3DLfC4D lights/source/LightCardTranslator.h
@@ -7,8 +7,8 @@
 
 class LightCardTranslator : public DL_TranslatorPlugin{
 public:
-	virtual void CreateNSINodes(const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
-	virtual void SampleMotion(DL_SampleInfo* info, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser);
+	virtual void CreateNSINodes(const char* Handle, const char* ParentTransformHandle, GeListNode* C4DNode, BaseDocument* doc, DL_SceneParser* parser);
+	virtual void SampleMotion(DL_SampleInfo* info, const char* Handle, GeListNode* C4DNode, BaseDocument* doc,DL_SceneParser* parser);
 
 	//virtual void CacheStaticData(BaseObject* obj, BaseDocument* doc, DL_SceneParser* parser);
 	//virtual void Emit(BaseObject* obj, BaseDocument* doc, DL_SceneParser* parser);

commit 136074885cc7bb522a343eb2633f14117fcde4b2

Fixed Shader Translator. This was broken by commit d783c921ae229c123f8f5cd1a276cd4f60b3801d

3DLfC4D/source/SceneParser.cpp
@@ -558,7 +558,9 @@ void SceneParser::TraverseShaders(BaseShader* shader, BaseDocument* doc){
 	DL_Translator* translator = n.GetTranslator();
 	if (translator){
 		nodes.push_back(n);
-		translator->CreateNSINodes("", "",shader, doc, this);
+		string handle = GetHandleName(shader);
+		translator->Init(shader, doc, this);
+		translator->CreateNSINodes(handle.c_str(), "",shader, doc, this);
 	}
 
 	TraverseShaders(shader->GetDown(),doc);