FMalmberg/3Delight-for-Cinema-4D

commit f761ef0142acedd17fa5f65e502ec6b22010a18f

Edited C4D menu to add 3Delight components (Materials, Objects etc.) 3Delight parameters will be added from the plugins SubMenu and will be added into a new SubMenu called 3Delight

3DLfC4D/source/main.cpp
@@ -13,6 +13,92 @@
 #include <windows.h>
 
 
+void Create3DelightMenu(BaseContainer* bc)
+{
+	if (!bc) return;
+	BrowseContainer browse(bc);
+	Int32 id = 0;
+	GeData * dat = nullptr;
+
+	while (browse.GetNext(&id, &dat))
+	{
+		if (id == MENURESOURCE_SUBMENU)
+		{
+			BaseContainer* container = dat->GetContainer();
+			String subtitle = container->GetString(MENURESOURCE_SUBTITLE);
+			if (container && subtitle == "IDS_EDITOR_PLUGINS")
+			{
+				BaseContainer sc;
+				sc.InsData(MENURESOURCE_SUBTITLE, String("3Delight"));
+
+				BaseContainer MaterialsSubMenu;
+				MaterialsSubMenu.InsData(MENURESOURCE_SUBTITLE, String("Materials"));
+				MaterialsSubMenu.InsData(MENURESOURCE_COMMAND, String("PLUGIN_CMD_1052718")); //DL_Principled
+				MaterialsSubMenu.InsData(MENURESOURCE_COMMAND, String("PLUGIN_CMD_1052717")); //DL_Glass
+				MaterialsSubMenu.InsData(MENURESOURCE_COMMAND, String("PLUGIN_CMD_1052719")); //DL_HAIRANDFUR
+				MaterialsSubMenu.InsData(MENURESOURCE_COMMAND, String("PLUGIN_CMD_1052720")); //DL_METAL
+				MaterialsSubMenu.InsData(MENURESOURCE_COMMAND, String("PLUGIN_CMD_1052721")); //DL_SKIN
+				MaterialsSubMenu.InsData(MENURESOURCE_COMMAND, String("PLUGIN_CMD_1052722")); //DL_SUBSTANCE
+				MaterialsSubMenu.InsData(MENURESOURCE_COMMAND, String("PLUGIN_CMD_1052724")); //DL_CARPAINT
+
+
+				BaseContainer ObjectsSubMenu;
+				ObjectsSubMenu.InsData(MENURESOURCE_SUBTITLE, String("Objects"));
+				ObjectsSubMenu.InsData(MENURESOURCE_COMMAND, "PLUGIN_CMD_" + String::IntToString(ID_ENVIRONMENTLIGHT));
+				ObjectsSubMenu.InsData(MENURESOURCE_COMMAND, "PLUGIN_CMD_" + String::IntToString(DL_OPENVDB));
+
+
+				BaseContainer LightsSubMenu;
+				LightsSubMenu.InsData(MENURESOURCE_SUBTITLE, String("Lights"));
+				LightsSubMenu.InsData(MENURESOURCE_COMMAND, "PLUGIN_CMD_" + String::IntToString(ID_LIGHTCARD));
+				LightsSubMenu.InsData(MENURESOURCE_COMMAND, "PLUGIN_CMD_" + String::IntToString(ID_DIRECTIONAL_LIGHT));
+				LightsSubMenu.InsData(MENURESOURCE_COMMAND, "PLUGIN_CMD_" + String::IntToString(ID_POINTLIGHT));
+				LightsSubMenu.InsData(MENURESOURCE_COMMAND, "PLUGIN_CMD_" + String::IntToString(ID_INCANDESCENCELIGHT));
+
+				BaseContainer RenderSubMenu;
+				RenderSubMenu.InsData(MENURESOURCE_SUBTITLE, String("Render"));
+				RenderSubMenu.InsData(MENURESOURCE_COMMAND, "PLUGIN_CMD_" + String::IntToString(ID_RENDER_COMMAND));
+				RenderSubMenu.InsData(MENURESOURCE_COMMAND, "PLUGIN_CMD_" + String::IntToString(ID_INTERACTIVE_RENDERING_START));
+				RenderSubMenu.InsData(MENURESOURCE_COMMAND, "PLUGIN_CMD_" + String::IntToString(ID_INTERACTIVE_RENDERING_STOP));
+				
+				sc.InsData(MENURESOURCE_SUBMENU, MaterialsSubMenu);
+				sc.InsData(MENURESOURCE_SUBMENU, ObjectsSubMenu);
+				sc.InsData(MENURESOURCE_SUBMENU, LightsSubMenu);
+				sc.InsData(MENURESOURCE_SUBMENU, RenderSubMenu);
+
+				bc->InsDataAfter(MENURESOURCE_STRING, sc, dat);
+
+			}
+			//ApplicationOutput(subtitle);
+		}
+	}
+}
+
+//
+//void AddSubMenus(BaseContainer* bc)
+//{
+//	if (!bc) return;
+//	BrowseContainer browse(bc);
+//	Int32 id = 0;
+//	GeData * dat = nullptr;
+//
+//	while (browse.GetNext(&id, &dat))
+//	{
+//		
+//			BaseContainer* container = dat->GetContainer();
+//			String subtitle = container->GetString(MENURESOURCE_SUBTITLE);
+//			if (subtitle == "3Delight")
+//			{
+//				AddSubMenus(dat->GetContainer());
+//			}
+//			//if(dat)
+//			//ApplicationOutput(dat->GetString());
+//	}
+//}
+
+
+
+
 //Global plugin manager, 
 //Stores pointers to allocators for translator plugins	
 
@@ -33,6 +119,7 @@ Bool RegisterInteractiveRenderingStart(void);
 Bool RegisterInteractiveRenderingStop(void);
 Bool RegisterInteractiveRenderManager(void);
 Bool Register3DelightCommand(void);
+Bool RegisterUserGuide(void);
 
 Bool PluginStart(void)
 {
@@ -56,11 +143,8 @@ Bool PluginStart(void)
 	if (!RegisterInteractiveRenderingStop()) return FALSE;
 	if (!Register3DelightPlugin()) return FALSE;
 	if (!RegisterInteractiveRenderManager()) return FALSE;
-	//if (!RegisterInteractiveRenderManager())return FALSE;
-	//if (!RegisterDisplay()) return FALSE;
-	if (!RegisterDL_CameraTag()) return FALSE;
-	if (!RegisterDL_MotionBlurTag()) return FALSE;
 	if (!Register3DelightCommand()) return FALSE;
+	if (!RegisterUserGuide()) return FALSE;
 
 	
 	if (!RegisterDisplay()) return FALSE;
@@ -105,6 +189,7 @@ Bool PluginMessage(Int32 id, void* data)
 		break;
 	}
 	case DL_LOAD_PLUGINS:
+	{
 		DL_PluginManager* pm = (DL_PluginManager*)data;
 		pm->RegisterHook(AllocateHook<CameraHook>);
 		pm->RegisterHook(AllocateHook<RenderOptionsHook>);
@@ -112,8 +197,18 @@ Bool PluginMessage(Int32 id, void* data)
 		pm->RegisterTranslator(ID_DL_VISIBILITYTAG, AllocateTranslator<VisibilityTagTranslator>);
 		//pm->RegisterTranslator(,AllocateTranslator<QualityValuesParser>);
 		break;
+	}
+	
+	case C4DPL_BUILDMENU:
+	{
+		BaseContainer* bc = GetMenuResource("M_EDITOR"_s);
+		if (!bc) return FALSE;
+		Create3DelightMenu(bc);
+		break;
+	}
 
 	}
 
+	
 	return false;
 }

3DLfC4D/source/IDs.h
@@ -17,9 +17,18 @@
 #define ID_HIERARCHY_TEST 1048613497
 #define ID_INTERACTIVE_RENDERING_START 1349865162 
 #define ID_INTERACTIVE_RENDERING_STOP 1349865163
+#define ID_RENDER_COMMAND 1349865166
 
 //Message IDs
 #define DL_START_INTERACTIVE_RENDERING 1349865164
 #define DL_STOP_INTERACTIVE_RENDERING 1349865165
-#define DL_RENDER_COMMAND 1349865166
+
+#define ID_LIGHTCARD 1041233 
+#define ID_INCANDESCENCELIGHT 104123387 
+#define ID_ENVIRONMENTLIGHT 1041234 
+#define ID_POINTLIGHT 10412398 //Get from cafe
+#define ID_DIRECTIONAL_LIGHT 1052934 //Get from cafe
+#define ID_ENVIRONMENTLIGHT 1041234 
+#define DL_OPENVDB	10528278
+
 

commit 63f700bd2898c777ddce0f5bb78eb6308566a367

Fixed minor build errors. Broken by commit f761ef0142acedd17fa5f65e502ec6b22010a18f

3DLfC4D/source/3DelightRendererCommand.cpp
@@ -86,6 +86,6 @@ Bool DL_3DelightRender_command::Execute(BaseDocument* doc)
 
 Bool Register3DelightCommand(void)
 {
-	return RegisterCommandPlugin(DL_RENDER_COMMAND, "3Delight Render"_s, PLUGINFLAG_HIDEPLUGINMENU, AutoBitmap("shelf_launchRender_200.png"_s), String("Render"_s), NewObjClear(DL_3DelightRender_command));
+	return RegisterCommandPlugin(ID_RENDER_COMMAND, "3Delight Render"_s, PLUGINFLAG_HIDEPLUGINMENU, AutoBitmap("shelf_launchRender_200.png"_s), String("Render"_s), NewObjClear(DL_3DelightRender_command));
 }
 

3DLfC4D/source/main.cpp
@@ -11,6 +11,7 @@
 #include "VisibilityTagTranslator.h"
 #include "IDs.h"
 #include <windows.h>
+#include "nsi_dynamic.hpp"
 
 
 void Create3DelightMenu(BaseContainer* bc)
@@ -30,7 +31,7 @@ void Create3DelightMenu(BaseContainer* bc)
 			{
 				BaseContainer sc;
 				sc.InsData(MENURESOURCE_SUBTITLE, String("3Delight"));
-
+						
 				BaseContainer MaterialsSubMenu;
 				MaterialsSubMenu.InsData(MENURESOURCE_SUBTITLE, String("Materials"));
 				MaterialsSubMenu.InsData(MENURESOURCE_COMMAND, String("PLUGIN_CMD_1052718")); //DL_Principled
@@ -67,40 +68,11 @@ void Create3DelightMenu(BaseContainer* bc)
 				sc.InsData(MENURESOURCE_SUBMENU, RenderSubMenu);
 
 				bc->InsDataAfter(MENURESOURCE_STRING, sc, dat);
-
 			}
 			//ApplicationOutput(subtitle);
 		}
 	}
 }
-
-//
-//void AddSubMenus(BaseContainer* bc)
-//{
-//	if (!bc) return;
-//	BrowseContainer browse(bc);
-//	Int32 id = 0;
-//	GeData * dat = nullptr;
-//
-//	while (browse.GetNext(&id, &dat))
-//	{
-//		
-//			BaseContainer* container = dat->GetContainer();
-//			String subtitle = container->GetString(MENURESOURCE_SUBTITLE);
-//			if (subtitle == "3Delight")
-//			{
-//				AddSubMenus(dat->GetContainer());
-//			}
-//			//if(dat)
-//			//ApplicationOutput(dat->GetString());
-//	}
-//}
-
-
-
-
-//Global plugin manager, 
-//Stores pointers to allocators for translator plugins	
 
 PluginManager PM;
 
@@ -119,21 +91,23 @@ Bool RegisterInteractiveRenderingStart(void);
 Bool RegisterInteractiveRenderingStop(void);
 Bool RegisterInteractiveRenderManager(void);
 Bool Register3DelightCommand(void);
-Bool RegisterUserGuide(void);
 
 Bool PluginStart(void)
 {
 	const char *env = getenv("DELIGHT");
-	if (env != nullptr)
+	std::string _3delight_path(env);
+	_3delight_path += "/bin/3Delight.DLL";
+		
+	NSI::DynamicAPI nsi_api = NSI::DynamicAPI(_3delight_path.c_str());
+	NSIContext_t     nsi_ctx;
+	bool all_good = false;
+	nsi_ctx = nsi_api.NSIBegin(0, nullptr);
+	if (nsi_ctx != NSI_BAD_CONTEXT)
 	{
-		std::string _3delight_path(env);
-		_3delight_path += "/bin/3Delight.DLL";
-		LoadLibraryA(_3delight_path.c_str());
+		nsi_api.NSIEnd(nsi_ctx);
+		all_good = true;
 	}
-	else
-	{
-		MessageDialog("Could not load 3Delight Library"_s);
-	}
+
 	//if (!RegisterCustomGUITest()) return FALSE;
 	if (!RegisterRenderFrame()) return FALSE;
 	if (!RegisterRenderSequence()) return FALSE;
@@ -144,7 +118,6 @@ Bool PluginStart(void)
 	if (!Register3DelightPlugin()) return FALSE;
 	if (!RegisterInteractiveRenderManager()) return FALSE;
 	if (!Register3DelightCommand()) return FALSE;
-	if (!RegisterUserGuide()) return FALSE;
 
 	
 	if (!RegisterDisplay()) return FALSE;